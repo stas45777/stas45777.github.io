1971ddcb81efa0b6fd5643d67a6319ef8be0c184
stas4577 <91030953+stas45777@users.noreply.github.com>
2021-11-14 16:03:53 +0300

    Update script.js

--- achiev/js/script.js
+++ achiev/js/script.js
@@ -135,4 +135,20 @@ $(document).ready(function(){
  $('.window__close').on('click',hideWindow)
  $('.window__bg').on('click',hideWindow)
  $('.show_window').on('click',showWindow)
+ // Настройка хедера
+ $('.button__menu_active').removeClass('button__menu_active')
+ $('.button__menu').eq(2).addClass('button__menu_active')
+
+ $('.button__menu').on('mouseenter',function (e) {
+		if ($(this).hasClass('button__menu_active') == true) {
+			return false
+		}
+		$('.button__menu_active').css('background','#fff')
+		$('.button__menu_active').css('color','#585858')
+	})
+	$('.button__menu').on('mouseleave',function () {
+		$('.button__menu_active').css('background','#ff5d54')
+		$('.button__menu_active').css('color','#fff')
+	})
+
 })
