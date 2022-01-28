5233b31f0720d6049a298e32adc21e345807a938
stas45777 <91030953+stas45777@users.noreply.github.com>
2022-01-28 15:32:36 +0300

    asd

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-
-
-$(document).ready(function(){
-	// Настройка высоты главного блока
-	let window_height = $(window).height()
-	$('.main').height(window_height - $('.header').outerHeight())
-})
-
